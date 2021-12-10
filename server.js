129f22cd7515e11fa9ed4afcde6e53ddc41c2ac2
tkodemo <93020429+tkodemo@users.noreply.github.com>
2021-12-09 17:45:02 -0700

    s

--- server.js
+++ server.js
@@ -23,7 +23,8 @@ var socketID = "";
 var users = [];
 
 //var mainURL = "http://csc337_final_proj:8080";
-var mainURL = "http://localhost:8080";
+//var mainURL = "http://localhost:8080";
+var mainURL = "http://137.184.154.123:8080";
 
 socketIO.on("connection", function (socket) {
 	console.log("User connected", socket.id);
